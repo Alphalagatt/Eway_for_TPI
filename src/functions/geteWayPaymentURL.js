f2c191b8619e12a76ddd66378d1565bf4f3a2aa1
Alpha Lagatt <lagattalpha@gmail.com>
2025-01-28 16:20:36 +1100

    added ecrypt

--- src/functions/geteWayPaymentURL.js
+++ src/functions/geteWayPaymentURL.js
@@ -1,7 +1,10 @@
 const { app } = require('@azure/functions');
+const scriptjs = require('scriptjs');
 const rapidEndpoint = "https://api.sandbox.ewaypayments.com/AccessCodesShared";
 const apiKey = "C3AB9ATDmEkBXOCFsXK81q+RG1X35oXI2z87aTJbaSWgJ5A9+OpX69eOLAquri7Gmqzwts";
 const password = "mJeH9HVn";
+
+
 
 app.http('geteWayPaymentURL', {
     methods: ['POST'],
@@ -9,39 +12,57 @@ app.http('geteWayPaymentURL', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        try {
-            //const name = request.query.get('name') || await request.text() || 'world';
-            const response = await fetch('https://api.ewaypayments.com/AccessCodesShared', {
-                method: 'POST',
-                mode: 'no-cors', // no-cors, *cors, same-origin
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Basic ' + Buffer.from(apiKey + ':' + password).toString('base64'),
-                },
-                body: JSON.stringify({
-                    "Payment": {
-                        "TotalAmount": 100
+        scriptjs('https://secure.ewaypayments.com/scripts/eCrypt.js', async function () {
+            try {
+                //const name = request.query.get('name') || await request.text() || 'world';
+                const response = await fetch('https://api.ewaypayments.com/AccessCodesShared', {
+                    method: 'POST',
+                    mode: 'no-cors', // no-cors, *cors, same-origin
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Basic ' + Buffer.from(apiKey + ':' + password).toString('base64'),
                     },
-                    "RedirectUrl": "http://www.eway.com.au",
-                    "CancelUrl": "http://www.eway.com.au",
-                    "TransactionType": "Purchase"
-                })
-            });
-            
+                    body: JSON.stringify({
+                        "Payment": {
+                            "TotalAmount": 100
+                        },
+                        "RedirectUrl": "http://www.eway.com.au",
+                        "CancelUrl": "http://www.eway.com.au",
+                        "TransactionType": "Purchase"
+                    })
+                });
 
-            const data = await response.json();
-            context.log(JSON.stringify(data));
-            return {
-                status: 200,
-                body: JSON.stringify(data)
-            };
 
-        } catch (err) { 
-            return {
-                status: 500,
-                body: err  
+                const data = await response.json();
+                context.log(JSON.stringify(data));
+
+                var eWAYConfig = {
+                    sharedPaymentUrl: data.SharedPaymentUrl,
+                };
+
+                console.log(eWAYConfig.sharedPaymentUrl);
+
+                function resultCallback(result, transactionID, errors) {
+                    if (result == "Complete") {
+                        alert("Payment complete! eWAY Transaction ID: " + transactionID);
+                    } else if (result == "Error") {
+                        alert("There was a problem completing the payment: " + errors);
+                    }
+                }
+                eCrypt.showModalPayment(eWAYConfig, resultCallback);
+
+                return {
+                    status: 200,
+                    body: JSON.stringify(data)
+                };
+
+            } catch (err) {
+                return {
+                    status: 500,
+                    body: err
+                };
             };
-        };
+        });
 
 
     }
